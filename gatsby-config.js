ee5718b8d86ee3ade0346eeb820a2ed927f9d78d
David Leung <david@davidslab.com>
2023-02-26 11:05:59 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Volla Solutions`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-google-gtag"]
+};
